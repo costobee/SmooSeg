eb119c00ae172296bc7541ae54241b244fe849eb
costobee <costo7620@gmail.com>
2024-11-21 19:07:21 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -10,7 +10,7 @@ class DinoFeaturizer(nn.Module):
         self.cfg = cfg
         self.dim = dim     # dimensionality of the output feature vector
         patch_size = self.cfg.dino_patch_size
-        self.patch_size = patch_size
+        self.patch_size = 8
         self.feat_type = self.cfg.dino_feat_type
         arch = self.cfg.model_type
         self.model = vits.__dict__[arch](patch_size=patch_size, num_classes=0)
@@ -111,14 +111,18 @@ class Projection(nn.Module):
 
     def make_clusterer(self, in_channels):
         return torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels, self.dim, (1, 1))   # reducing channels of in_channels to dim using conv of kernel size 1*1
-        )
+            torch.nn.Conv2d(in_channels, self.dim, (1, 1)),
+            torch.nn.BatchNorm2d(self.dim),
+            torch.nn.SiLU())
+        
 
     def make_nonlinear_clusterer(self, in_channels):
         return torch.nn.Sequential(
             torch.nn.Conv2d(in_channels, in_channels, (1, 1)),
+            torch.nn.BatchNorm2d(in_channels),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(in_channels, self.dim, (1, 1)))
+            torch.nn.Conv2d(in_channels, self.dim, (1, 1)),
+            torch.nn.BatchNorm2d(self.dim))
 
 
     def forward(self, feat):
